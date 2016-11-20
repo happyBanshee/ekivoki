bc85bb67bd7f18abb0b343c5dabd35d8159262f4
happyBanshee <alter.koval.olga@gmail.com>
2016-11-20 18:22:05 +0100

    Start angular structure

---
+++ src/controllers/mainapp.controller.ts
@@ -0,0 +1,7 @@
+angular.module("MainApp")
+    .controller('MainAppController', mainAppController);
+
+mainAppController.$inject = ['$scope'];
+function mainAppController($scope) {
+
+}
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
 import * as angular from 'angular'
+import "./modules/mainapp.module";
+import "./controllers/mainApp.controller";
 
 console.log('HELLO!');
